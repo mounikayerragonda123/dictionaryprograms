2532b26e45b84902c86af7b38080d2b2fb67ba3f
mounikayerragonda123 <130558904+mounikayerragonda123@users.noreply.github.com>
2023-08-22 20:28:14 +0530

    Add files via upload

---
+++ randommodule_pgrms.py
@@ -0,0 +1,16 @@
+#programs on random module functions
+from random import*
+for i in range(10): 
+    print(random())
+for i in range(10): 
+    print(randint(0,9))
+for i in range(10): 
+    print(uniform(1,10))
+from random import*
+for i in range(10): 
+    print(randrange(0,9,2))
+l=['m','o','u','n','i','k','a']
+from random import*
+for i in range(10): 
+    print(choice(l))
+
